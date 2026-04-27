55ea773da724ed746ae6ee1b68b908b248aadfc0
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2026-04-27 18:57:31 -0300

    feat: crear servicio de socios con validaciones closes #27

---
+++ src/main/java/com/bibliotech/exception/ValidacionException.java
@@ -0,0 +1,8 @@
+package com.bibliotech.exception;
+
+public class ValidacionException extends BibliotecaException {
+
+    public ValidacionException(String mensaje) {
+        super(mensaje);
+    }
+}
