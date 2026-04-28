5e7af46f39fc04f6147e98b299594763650c0b1c
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2026-04-28 09:47:51 -0300

    chore: finalizacion del tp + documentacion de decisiones de diseño closes #35

---
+++ src/main/java/com/bibliotech/exception/LibroNoDisponibleException.java
@@ -0,0 +1,8 @@
+package com.bibliotech.exception;
+
+public class LibroNoDisponibleException extends BibliotecaException {
+
+    public LibroNoDisponibleException(String mensaje) {
+        super(mensaje);
+    }
+}
